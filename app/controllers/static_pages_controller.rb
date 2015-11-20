797346073c5ae3633795c00bcb224d3b7bb417d9
German Grandson <ganieto1@gmail.com>
2015-11-20 17:09:18 -0500

    modified params variables

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,15 +11,14 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    # render plain: params
-    unless params[:q] == ""
-      url = "https://maps.googleapis.com/maps/api/geocode/json?address="+params[:q]+"&key=YOUR KEY"
+    unless params[:chicken] == ""
+      url = "https://maps.googleapis.com/maps/api/geocode/json?address="+params[:chicken]+"&key=AIzaSyBITsYDCU8mfLW9exGEBrU6vy5xTNyV264="
       response = HTTParty.get(url)
       @latitude = response['results'][0]['geometry']['location']['lat']
       @longitude = response['results'][0]['geometry']['location']['lng']
-      url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+@latitude.to_s+",%20"+@longitude.to_s+"&radius=1000&types=bar&key=YOUR KEY"
+      url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+@latitude.to_s+",%20"+@longitude.to_s+"&radius=5000&types=bar&key=AIzaSyBITsYDCU8mfLW9exGEBrU6vy5xTNyV264="
       response = HTTParty.get(url)
-      
+
       @bars = response['results'].map do |bar|
         Venue.find_or_create_by(name: bar['name'], address: bar['vicinity']) do |venue|
           venue.name = bar['name']
@@ -29,9 +28,9 @@ class StaticPagesController < ApplicationController
           venue.lng = bar['geometry']['location']['lng']
         end
       end
-      
+
       flash.now[:success] = "Success!"
-    else 
+    else
       flash.now[:failure] = "Search cannot be empty"
     end
     render "home"
