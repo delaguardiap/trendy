60d969f49af8238f93a7ddd33e560efb153e27b8
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-27 13:30:52 -0500

    Add staticpages helper method

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,7 +2,7 @@ module StaticPagesHelper
 	def getTweets venue
 		tweets = venue.name.gsub(' ','%20')
     tweets = tweets.gsub(',','%2C')
-    response = HTTParty.post("https://api.twitter.com/oauth2/"+ENV['twitterAPI']+":"+ENV['twitterSecret'])
-    response
+    response = HTTParty.post("")
+    response.to_json
 	end
 end
