8de3214bbd3b9884608e3b3d0ce6c379fc0e7bb0
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-28 16:36:47 -0500

    Add slider for rating

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,15 @@
 module StaticPagesHelper
+	def getTweets venue
+	
+	config = {
+    consumer_key:    ENV["twitterAPI"],
+    consumer_secret: ENV["twitterSecret"],
+  }
+
+  client = Twitter::REST::Client.new(config)  
+  result = client.search(venue.name, result_type: "recent")
+  result.count
+  result.to_json
+	end
+
 end
