fa8852a1633cf4b5ed8bc37e1a4fd8d724d05bff
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-27 20:01:44 -0500

    Move the getTweets method from static pages helper to venues helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,14 +1,2 @@
 module StaticPagesHelper
-	def getTweets venue
-	
-	config = {
-    consumer_key:    ENV["twitterAPI"],
-    consumer_secret: ENV["twitterSecret"],
-  }
-
-  client = Twitter::REST::Client.new(config)  
-  result = client.search(venue.name, result_type: "recent").take(100)
-  result.count
-	end
-
 end
