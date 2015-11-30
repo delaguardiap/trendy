5320b77f72661a42dbe68c79965326a78908e78f
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-29 21:56:46 -0500

    Add styling to home page

--- app/helpers/check_ins_helper.rb
+++ app/helpers/check_ins_helper.rb
@@ -1,4 +1,5 @@
 module CheckInsHelper
+	
 	def lastCheckInValid
 		if current_user
 		current_user.check_ins.last && current_user.check_ins.last.active == true && current_user.check_ins.last.created_at > 5.hours.ago
