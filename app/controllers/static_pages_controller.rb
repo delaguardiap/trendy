15b7fd6ad2a989e94b69dd90f9bcb5fa36064759
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-30 18:00:14 -0500

    Fix dropdowns

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,30 +12,35 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    unless params[:chicken] == ""
+    if params[:chicken] != ""
       address = params[:chicken].gsub(' ','%20')
       address = address.gsub(',','%2C')
       url = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key=#{ENV['googlekey']}"
       response = HTTParty.get(url)
-      @latitude = response['results'][0]['geometry']['location']['lat']
-      @longitude = response['results'][0]['geometry']['location']['lng']
-      url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+@latitude.to_s+",%20"+@longitude.to_s+"&radius=5000&types=night_club&key=#{ENV['googlekey']}"
-      response = HTTParty.get(url)
-      @bars = response['results'].map do |bar|
-        Venue.find_or_create_by(name: bar['name'], address: bar['vicinity']) do |venue|
-          venue.name = bar['name']
-          venue.address = bar['vicinity']
-          venue.rating = bar['rating']
-          venue.price = bar['price_level']
-          venue.lat = bar['geometry']['location']['lat']
-          venue.lng = bar['geometry']['location']['lng']
+      if response['results'][0] == nil
+        flash.now[:failure] = "Please search for an area or zip code, e.g: Miami Beach, or 33139"
+      else
+        @latitude = response['results'][0]['geometry']['location']['lat']
+        @longitude = response['results'][0]['geometry']['location']['lng']
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+@latitude.to_s+",%20"+@longitude.to_s+"&radius=5000&types=night_club&key=#{ENV['googlekey']}"
+        response = HTTParty.get(url)
+        @bars = response['results'].map do |bar|
+          Venue.find_or_create_by(name: bar['name'], address: bar['vicinity']) do |venue|
+            venue.name = bar['name']
+            venue.address = bar['vicinity']
+            venue.rating = bar['rating']
+            venue.price = bar['price_level']
+            venue.lat = bar['geometry']['location']['lat']
+            venue.lng = bar['geometry']['location']['lng']
+          end
         end
       end
     else
       flash.now[:failure] = "Search cannot be empty"
     end
+    @search = params[:chicken]
     render "home"
-  end
+end
 
 
 
