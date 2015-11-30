1705ca92b60ea04fc3758c03386970f2a3fb1788
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-30 14:55:14 -0500

    More styling

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -12,4 +12,16 @@ module StaticPagesHelper
   result.to_json
 	end
 
+  def getTweetsContent venue
+  
+  config = {
+    consumer_key:    ENV["twitterAPI"],
+    consumer_secret: ENV["twitterSecret"],
+  }
+
+  client = Twitter::REST::Client.new(config)  
+  result = client.search(venue.name, result_type: "recent")
+  end
+
+
 end
