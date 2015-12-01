9556c3577206eb36077a053ac0e60d240d48426d
German Grandson <ganieto1@gmail.com>
2015-12-01 14:20:25 -0500

    styling

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -3,3 +3,25 @@
 // 		console.log("Good");
 // 	});
 // });
+
+//Your Location ---------------------------
+function theFunction() {
+ console.log("I'm working!");
+};
+
+var map;
+
+function initMap() {
+
+ if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position){
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    console.log(lat);
+    console.log(lon);
+
+ var pyrmont = {lat: lat, lng: lon};
+
+ });
+};
+};
