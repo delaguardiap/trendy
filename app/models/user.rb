3f3ece1a44518de5278e3044e6b0aea99441e2f4
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-12-01 09:47:18 -0500

    Create friendship relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
 
   has_many :check_ins
   has_many :upvotes
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
   validates :name, presence: true, length: { maximum: 50 }
 	validates :email, email: true, uniqueness: true
@@ -19,6 +28,24 @@ class User < ActiveRecord::Base
     end
   end
 # user.check_in!(venue)
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    CheckIn.where("user_id IN (#{following_ids}) OR user_id = :user_id", 
+                  following_ids: following_ids, user_id: id)
+  end
 
   def check_in!(venue, review="", rating=0)
     deactivate_check_ins!
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129211617) do
+ActiveRecord::Schema.define(version: 20151201131319) do
 
   create_table "check_ins", force: :cascade do |t|
     t.integer  "user_id"
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 20151129211617) do
   end
 
   add_index "downvotes", ["check_in_id"], name: "index_downvotes_on_check_in_id"
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
